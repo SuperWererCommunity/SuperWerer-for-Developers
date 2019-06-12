d134bcd998495004b1ed68befce555869b14dcf2
avarise <parasyte217@gmail.com>
2019-06-12 01:08:08 -0700

    Properly styled everything

--- developers.php
+++ developers.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
  ?>
+
+<!-- HTML -->
 <!DOCTYPE HTML>
 <html>
 
@@ -11,38 +13,52 @@ session_start();
 <title> SW for Developers v1.0 BETA </title>
 </head>
 
+<header>
+
+  <h3>SuperWerer</h3><p>for Developers</p>
+</header>
+
 
 <body oncontextmenu="return false;">
-
-<canvas id="canvas1" width="800" height="600" class="mainborder"> </canvas>
-
-<div style="top: 30px; left: 20px; position: absolute;"> SuperWerer <br> for Developers </div>
-
-<div style="top:30px; left: 620px; position: absolute;"> Version 0.0.1 BETA </div>
-<br>
-<br>
-
-<div>
+<!-- Login form -->
 
 <form id="overlay" method="post">
-<input id="input1" style="position: absolute;top: 250px; left: 200px; " type="text" name="usd" placeholder="Username">
-<input id="input2" style="position: absolute;top: 250px; left: 400px; " type="password" name="pwd" placeholder="Password">
-<button id="input3" style="position: absolute;top: 300px; left: 360px; " type="submit" name="login-submit">Login</button>
+  <v-box>
+  <v-box class="loginForm">
+  <img src="./images/SW_logo_4.png"/>
+    <h1>LOG IN</h1>
+
+    <v-box class="inputs">
+      <input id="input1" type="text" name="usd" placeholder="Forum Username"/>
+      <input id="input2" type="password" name="pwd" placeholder="Forum Password"/>
+      <h-box>
+        <div id="forgotpassword">
+          <a href="https://superwerer.com/member.php?action=lostpw" target="_blank"> Forgot password? </a>
+        </div>
+      </h-box>
+      <button id="input3" type="submit" name="login-submit">Login</button>
+    </v-box>
+
+    <div id="logininfo"> 
+      <p>Don't have a forums account yet? 
+        <a href="https://superwerer.com/member.php?action=register" target="_blank">Register here!</a>
+      </p>
+    </div>
+  </v-box>
+  </v-box>
 </form>
 
-</div>
 
-<div id="logininfo" style="position: absolute;top: 350px;left:200px;"> Login using your forum username and password! <br>
-  Don't have a forums account yet? <a href="https://superwerer.com/member.php?action=register" target="_blank"> Register here! </a>
- </div>
 
- <div id="forgotpassword" style="position:absolute;top:275px;left:420px;">
-   <a style="text-decoration:none;" href="https://superwerer.com/member.php?action=lostpw" target="_blank"> Forgot your password? </a>
- </div>
-
- <div id="reportbug" style="position: absolute;top: 580px;left:650px;font-size:25px;">
+<footer>
+  <p> Version 0.0.1 BETA </p>
+  <div id="reportbug">
     <a href="https://superwerer.com/form.php?formid=6" target="_blank"> Report Bug </a>
   </div>
+</footer>
+
+
+<!-- END HTML -->
 
 
   <?php
@@ -55,7 +71,7 @@ session_start();
   reportbug.style.display='block';
 
   var forgotpassword = document.getElementById('forgotpassword');
-  forgotpassword.style.display='block';
+  forgotpassword.style.display='flex';
 
   </script>";
 
